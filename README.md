36d443b1ed0506ff320a5afd34f14adbe6c80c42
TheBharatMishra <mishrabm2004@gmail.com>
2024-01-21 19:30:51 +0530

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JavaL
+
 Learning Java Programming Language.
+
+##
+
+## Data Types
+
+## Wrapper Classes
+
+They provide a way to use primitive data types as objects.
---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+public static void main(String args[]){
+    int a = 10;
+    float f = 10.5f;
+    System.out.println(a);        
+    System.out.println(f);       
+ }}
+    
+
