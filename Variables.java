582ab3fdee283461f08d875fc1e8a86cc58d9119
TheBharatMishra <mishrabm2004@gmail.com>
2024-04-13 00:50:30 +0530

    Basic

--- Variables.java
+++ Variables.java
@@ -2,8 +2,13 @@ public class Variables {
 public static void main(String args[]){
     int a = 10;
     float f = 10.5f;
-    System.out.println(a);        
-    System.out.println(f);       
+    double d = 69.420;
+    boolean isB = false;
+    var v="Bhanu";
+    int numbers[]={2,3,4,5};
+    System.out.println(Double.toString(d)+v);        
+    System.out.println(f);
+    System.out.println(numbers);
  }}
     
 
